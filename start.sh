51cb7773f1db7bc0f41203a6c4a9bab3995c7b1f
TeamShizu <87710820+TeamShizu@users.noreply.github.com>
2021-11-01 13:21:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/TeamShizu/MusicV1 /MusicV1
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/TeamShizu/MusicV1 -b $BRANCH /MusicV1
 fi
-cd /VCPlayerBot
+cd /MusicV1
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
